bcb572bed7e9ec0527b8f79703af3d3f73ad1c67
RahulM222 <rahul.medappa1@gmail.com>
2024-03-31 21:35:18 +0100

    Final Message Generator

--- RandomMessage.js
+++ RandomMessage.js
@@ -1,1 +1,20 @@
-console.log('Hello World')
+
+invType = ['bonds', 'stocks and shares', 'private capital'];
+
+estReturn = Math.floor(Math.random() * 100);
+
+successLevel = ['reasonable', 'very good', 'high'];
+
+const investmentSuccess = () => {
+    selectedType = invType[Math.floor(Math.random() * 3)]
+    if (estReturn > 0 && estReturn < 10) {
+        selectedLevel = successLevel[0];
+    } else if (estReturn > 10 && estReturn < 20) {
+        selectedLevel = successLevel[1];
+    } else {
+        selectedLevel = successLevel[2];
+    }
+    return 'You invested in ' + selectedType + ' and achieved a ' + selectedLevel + ' return of ' + estReturn + '% per annum.'
+}
+
+console.log(investmentSuccess());
